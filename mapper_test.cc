af51f1350feca07135a8e2bde5cd9ef237098070
David Banks <amoebae@gmail.com>
2017-12-04 17:35:42 +0000

    mapping demo

--- mapper_test.cc
+++ mapper_test.cc
@@ -1,8 +1,41 @@
 #include <gmock/gmock.h>
+#include <QJsonObject>
+#include <QJsonDocument>
 
 using ::testing::Eq;
 
-TEST(MyComponent, ActsAsIExpect) {
-    ASSERT_THAT(2 + 2, Eq(4));
+class ArticleCreationRequest {
+public:
+    ArticleCreationRequest(std::string title) : title(title) { }
+    std::string toJson();
+private:
+    std::string title;
+};
+
+std::string ArticleCreationRequest::toJson() {
+    QJsonObject object;
+    QJsonValue titleVal(QString::fromStdString(this->title));
+
+    object.insert("title", titleVal);
+
+    QString result = QString::fromUtf8(QJsonDocument(object).toJson());
+
+    return result.toStdString();
+}
+
+QJsonObject deserialize(std::string input) {
+    auto result = QJsonDocument::fromJson(QString::fromStdString(input).toUtf8());
+    return result.object();
+}
+
+
+TEST(ArticleCreationRequestTest, SerializesToJson) {
+    ArticleCreationRequest request("To Serve Man");
+    
+    std::string serializedResult = request.toJson();
+        
+    std::string expectedResult = "{\"title\": \"To Serve Man\"}";
+
+    ASSERT_THAT(deserialize(serializedResult), Eq(deserialize(expectedResult)));
 }
 
