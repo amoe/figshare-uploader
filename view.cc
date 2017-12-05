af189a56a291f0c8f2af45f3899ae82555b343ef
David Banks <amoebae@gmail.com>
2017-12-05 16:37:09 +0000

    some comments

--- view.cc
+++ view.cc
@@ -9,8 +9,9 @@
 View::View(Presenter presenter) : QWidget(0, Qt::Window), presenter(presenter) {
     // layout MUST be allocated on the heap here, if it goes out of scope, it
     // will simply stop layout out the widgets properly!
-
-    QGridLayout* layout = new QGridLayout;
+    // But, I think that we don't need to clean it up because the QWidget
+    // destructor will delete it at the appropriate time.
+    QGridLayout* layout = new QGridLayout(this);
 
     this->selectedFile = new QLineEdit(this);
     layout->addWidget(this->selectedFile, 0, 0);
