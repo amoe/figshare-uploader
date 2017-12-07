5caa5e6f74e4bcd5de25fb7643b501786d6c50d1
David Banks <amoebae@gmail.com>
2017-12-07 10:57:08 +0000

    category response

--- raw_literals.hh
+++ raw_literals.hh
@@ -2,6 +2,54 @@
 // 
 
 namespace raw_literals {
+    std::string categoryResponse = R"V0G0N(
+        {
+            "id": 1699,
+            "parent_id": 1138,
+            "title": "Hospitality Management"
+        },
+        {
+            "id": 1700,
+            "parent_id": 1138,
+            "title": "Real Estate and Valuation Services"
+        },
+        {
+            "id": 1701,
+            "parent_id": 1138,
+            "title": "Sport and Leisure Management"
+        },
+        {
+            "id": 1702,
+            "parent_id": 50,
+            "title": "Maori History"
+        },
+        {
+            "id": 1703,
+            "parent_id": 50,
+            "title": "Middle Eastern and African History"
+        },
+        {
+            "id": 1704,
+            "parent_id": 50,
+            "title": "New Zealand History"
+        },
+        {
+            "id": 1705,
+            "parent_id": 50,
+            "title": "North American History"
+        },
+        {
+            "id": 1706,
+            "parent_id": 50,
+            "title": "Commercial and Contract Law"
+        },
+        {
+            "id": 1707,
+            "parent_id": 50,
+            "title": "Comparative Law"
+        }
+    )";
+
     std::string expectedResult = R"V0G0N(
         {
             "title": "To Serve Man",
