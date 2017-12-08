ba3dee3b3ee025838a7a70a65a8272c9db138519
David Banks <amoebae@gmail.com>
2017-12-08 10:35:06 +0000

    unit tests for post function

--- test_utility.cc
+++ test_utility.cc
@@ -1,8 +1,20 @@
+#include <string>
 #include <QString>
 #include <QJsonDocument>
 #include <QJsonObject>
 
+using std::string;
+
 QJsonObject deserialize(const std::string input) {
     auto result = QJsonDocument::fromJson(QString::fromStdString(input).toUtf8());
     return result.object();
+}
+
+string fetchString(
+    const string input, const string key
+) {
+    auto result = QJsonDocument::fromJson(QString::fromStdString(input).toUtf8());
+    const auto qKey = QString::fromStdString(key);
+    
+    return result.object().value(qKey).toString().toStdString();
 }
--- test_utility.hh
+++ test_utility.hh
@@ -1,3 +1,9 @@
+#include <string>
 #include <QJsonObject>
 
+using std::string;
+
 QJsonObject deserialize(const std::string input);
+string fetchString(
+    const string textRepresentation, const string key
+);
