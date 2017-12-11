8ab472d16f0de307991bc53ada5a4a4cd7bcc277
David Banks <amoebae@gmail.com>
2017-12-11 12:19:18 +0000

    formalizing tests for the part preparer

--- part_preparer_test.cc
+++ part_preparer_test.cc
@@ -3,24 +3,27 @@
 #include "io_slicer.hh"
 #include "part_preparer.hh"
 
+using ::testing::Eq;
+
 TEST(PartPreparerTest, ActsAsIExpect) {
     // Dependency
     // some issue here because the fileinfo needs to also contain the data...
 
-    IOSlicer *slicer = new FileSlicer("/home/amoe/cpp-netlib-build.tar");
+    IOSlicer *slicer = new FileSlicer("lena.jpg");
     PartPreparer preparer(slicer);
-    FileInfo fileInfo("http://my-site.com/", "/home/amoe/cpp-netlib-build.tar");
+    FileInfo fileInfo("http://my-site.com/", "lena.jpg");
 
     FilePart filePart(
         42, 2, 8, false, FilePartStatus::PENDING
     );
 
-    std::cout << "start offset is " << filePart.getStartOffset() << std::endl;
-    std::cout << "end offset is " << filePart.getEndOffset() << std::endl;
+    ASSERT_THAT(filePart.getStartOffset(), Eq(2));
+    ASSERT_THAT(filePart.getEndOffset(), Eq(8));
 
     UploadCommand result = preparer.prepareUpload(fileInfo, filePart);
 
-    std::cout << "upload url: " << result.getUrl() << std::endl;
-    std::cout << "upload size: " << result.getData().size() << std::endl;
+    ASSERT_THAT(result.getUrl(), Eq("lena.jpg/42"));
+    ASSERT_THAT(result.getData().size(), Eq(6));
+
 }
 
