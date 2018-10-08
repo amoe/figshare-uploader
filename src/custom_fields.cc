0c2ee5f9caa113dec4becac8f3aa884b282ff039
David Banks <amoebae@gmail.com>
2018-10-08 19:46:20 +0100

    new types

--- src/custom_fields.cc
+++ src/custom_fields.cc
@@ -1,2 +1,6 @@
 #include "custom_fields.hh"
 
+QJsonObject CustomFieldSet::render() const {
+    QJsonObject result;
+    return result;
+}
--- src/custom_fields.hh
+++ src/custom_fields.hh
@@ -1,7 +1,33 @@
 #ifndef CUSTOM_FIELDS_HH
 #define CUSTOM_FIELDS_HH
 
+#include <vector>
+#include <string>
 #include <QJsonObject>
+
+using std::string;
+using std::vector;
+
+enum class FieldEncoder {
+    STRING, LIST
+};
+
+struct CustomFieldSpecification {
+    string name;             // key that will be used in result
+    int position;            // column position to expect result at
+    FieldEncoder encoder;    // way the result will appear in the JSON
+};
+
+class CustomFieldDatum {
+public:
+    CustomFieldDatum(
+        CustomFieldSpecification specification, string content
+    ): specification(specification), content(content) { }
+
+private:
+    CustomFieldSpecification specification;
+    string content;
+};
 
 // The CustomFieldSet has to be copy-safe.  That means we can't use polymorphism
 // to implement the encoding behaviour.
@@ -11,6 +37,9 @@ public:
 
     void add();
     QJsonObject render() const;
+
+private:
+    vector<CustomFieldDatum> content;
 };
 
 #endif /* CUSTOM_FIELDS_HH */
