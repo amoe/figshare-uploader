6eb08d9ab00e8c5db238e208514b26257460cbb4
David Banks <amoebae@gmail.com>
2019-04-08 11:26:36 +0100

    correcting layout

--- src/settings_dialog.cc
+++ src/settings_dialog.cc
@@ -1,17 +1,25 @@
-#include <QDebug>
+#include <QTabWidget>
 #include <QStringList>
 #include <QStringListModel>
+#include <QVBoxLayout>
 #include "settings_dialog.hh"
 #include "field_encoder_list_view.hh"
 #include "mapping_table_widget.hh"
 
-SettingsDialog::SettingsDialog(QWidget* parent): QTabWidget(parent) {
+SettingsDialog::SettingsDialog(QWidget* parent): QDialog(parent) {
+    QTabWidget* container = new QTabWidget(this);
+        
     QStringList stringList = {"fry", "bender", "leela"};
     QStringListModel* theModel = new QStringListModel(stringList);
 
     MappingTableWidget* mappingTable = new MappingTableWidget(this, theModel);
     FieldEncoderListView* fieldEncoderList = new FieldEncoderListView(this, theModel);
 
-    addTab(mappingTable, "Field mapping");
-    addTab(fieldEncoderList, "Encoder configuration");
+    container->addTab(mappingTable, "Field mapping");
+    container->addTab(fieldEncoderList, "Encoder configuration");
+
+    QVBoxLayout* dialogLayout = new QVBoxLayout;
+    dialogLayout->addWidget(container);
+    
+    this->setLayout(dialogLayout);
 }
--- src/settings_dialog.hh
+++ src/settings_dialog.hh
@@ -2,9 +2,9 @@
 #define SETTINGS_DIALOG_HH
 
 #include <QWidget>
-#include <QTabWidget>
+#include <QDialog>
 
-class SettingsDialog: public QTabWidget {
+class SettingsDialog: public QDialog {
 public:
     SettingsDialog(QWidget* parent = nullptr);
 };
