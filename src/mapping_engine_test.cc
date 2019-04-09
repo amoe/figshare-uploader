dba070cb7d61fe5bedc469d0d3f404159d488afe
David Banks <amoebae@gmail.com>
2019-04-09 15:35:37 +0100

    skeleton for mapping engine

---
+++ src/mapping_engine_test.cc
@@ -0,0 +1,15 @@
+#include <gmock/gmock.h>
+
+using ::testing::Test;
+using ::testing::Eq;
+
+class MappingEngineTest: public Test {
+public:
+    MappingEngineTest() { }
+};
+
+
+TEST_F(MappingEngineTest, SanityCheck) {
+    ASSERT_THAT(2 + 2, Eq(4));
+}
+
