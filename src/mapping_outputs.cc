bb11d2901b6b4bad8333b3778fa36ccf8efd47cf
David Banks <amoebae@gmail.com>
2019-04-11 14:25:12 +0100

    backing out of this

--- src/mapping_outputs.cc
+++
@@ -1,9 +0,0 @@
-#include "mapping_outputs.hh"
-
-QJsonObject MappingOutput::getArticleObject() const {
-    return articleObject;
-}
-
-vector<string> MappingOutput::getSourcePaths() const {
-    return sourcePaths;
-}
