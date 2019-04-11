e861fba68882a83f8a3c3dab7aa49b219dc0df3c
David Banks <amoebae@gmail.com>
2019-04-11 17:06:18 +0100

    specify hashing

--- src/converter_registry.hh
+++ src/converter_registry.hh
@@ -7,6 +7,16 @@
 
 using std::unordered_map;
 
+// This is basically a hack to allow storing enum classes in an unordered_map,
+// and it should definitely be moved somewhere more useful.
+struct EnumClassHash {
+    template <typename T>
+    std::size_t operator()(T t) const {
+        return static_cast<std::size_t>(t);
+    }
+};
+
+
 class Converter {
 public:
     virtual IntermediateMappingOutput applyConversion(string input, OptionsMap options) = 0;
@@ -27,7 +37,7 @@ public:
     IntermediateMappingOutput convert(ConverterName c, string value, OptionsMap options);
 
 private:
-    unordered_map<ConverterName, Converter*> converterMap;
+    unordered_map<ConverterName, Converter*, EnumClassHash> converterMap;
 };
 
 #endif /* CONVERTER_REGISTRY_HH */
