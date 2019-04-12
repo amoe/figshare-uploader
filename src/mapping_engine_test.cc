fd0ce57aee6565017e608278145352a89d4d42ca
David Banks <amoebae@gmail.com>
2019-04-12 11:57:26 +0100

    adding red test

--- src/mapping_engine_test.cc
+++ src/mapping_engine_test.cc
@@ -16,7 +16,18 @@ using std::string;
 
 class MappingEngineTest: public Test {
 public:
-    MappingEngineTest() { }
+    MappingEngineTest() {
+        lookups = new LookupRegistryImpl;
+        engine = new MappingEngine(lookups);
+        
+    }
+    ~MappingEngineTest() {
+        delete engine;
+        delete lookups;
+    }
+
+    LookupRegistry* lookups;
+    MappingEngine* engine;
 };
 
 // It's quite difficult to construct these MappingSchemes in C++ code.
@@ -38,9 +49,7 @@ TEST_F(MappingEngineTest, TitleStringConversionCheck) {
 
     vector<string> theDocument = {"foo"};
 
-    LookupRegistry* lookups = new LookupRegistryImpl;
-    MappingEngine engine(lookups);
-    MappingOutput result = engine.convert(theDocument, theScheme);
+    MappingOutput result = this->engine->convert(theDocument, theScheme);
 
     const string expectedResult = R"(
         {
@@ -55,68 +64,68 @@ TEST_F(MappingEngineTest, TitleStringConversionCheck) {
 }
 
 
-// TEST_F(MappingEngineTest, ContributeFilesCheck) {
-//     vector<string> theDocument = {
-//         "foo.tiff;bar.tiff"
-//     };
-//     OptionsMap options = {
-//         {"delimiter", optional<string>(";")}
-//     };
+TEST_F(MappingEngineTest, ContributeFilesCheck) {
+    vector<string> theDocument = {
+        "foo.tiff;bar.tiff"
+    };
+    OptionsMap options = {
+        {"delimiter", optional<string>(";")}
+    };
 
-//     FieldEncoder contributeFilesEncoder(
-//         nullopt,
-//         ConverterName::CONTRIBUTE_FILES,
-//         {},
-//         options
-//     );
-//     MappingScheme theScheme = {RowMapping(0, contributeFilesEncoder)};
+    FieldEncoder contributeFilesEncoder(
+        nullopt,
+        ConverterName::CONTRIBUTE_FILES,
+        {},
+        options
+    );
+    MappingScheme theScheme = {RowMapping(0, contributeFilesEncoder)};
 
-//     MappingEngine engine;
-//     MappingOutput result = engine.convert(theDocument, theScheme);
+    MappingOutput result = this->engine->convert(theDocument, theScheme);
 
-//     // Expect an empty article object because we haven't defined any other
-//     // converters.
-//     QJsonObject expectedArticle;
-//     vector<string> expectedSourcePaths = {
-//         "foo.tiff", "bar.tiff"
-//     };
+    // Expect an empty article object because we haven't defined any other
+    // converters.
+    QJsonObject expectedArticle;
+    vector<string> expectedSourcePaths = {
+        "foo.tiff", "bar.tiff"
+    };
 
-//     ASSERT_THAT(result.getArticleObject(), Eq(expectedArticle));
-//     ASSERT_THAT(result.getSourcePaths(), Eq(expectedSourcePaths));
-// }
-
-
-// TEST_F(MappingEngineTest, DefinedTypeLookupListCheck) {
-//     vector<string> theDocument = {
-//         "Figure"
-//     };
-//     OptionsMap options = {
-//         {"resourceName", optional<string>("definedType")}
-//     };
-
-//     FieldEncoder lookupListEncoder(
-//         optional<string>("defined_type"),
-//         ConverterName::LOOKUP_LIST,
-//         {},
-//         options
-//     );
-//     MappingScheme theScheme = {RowMapping(0, contributeFilesEncoder)};
-
-//     MappingEngine engine;
-//     MappingOutput result = engine.convert(theDocument, theScheme);
-
-//     // Expect an empty article object because we haven't defined any other
-//     // converters.
-//     QJsonObject expectedArticle;
-//     vector<string> expectedSourcePaths = {};
-
-//     const string expectedResult = R"(
-//         {
-//            "defined_type": "figure"
-//         }
-//     )";
+    ASSERT_THAT(result.getArticleObject(), Eq(expectedArticle));
+    ASSERT_THAT(result.getSourcePaths(), Eq(expectedSourcePaths));
+}
 
 
-//     ASSERT_THAT(result.getArticleObject(), Eq(deserialize(expectedResult)));
-//     ASSERT_THAT(result.getSourcePaths(), Eq(expectedSourcePaths));
-// }
+TEST_F(MappingEngineTest, DefinedTypeLookupListCheck) {
+    vector<string> theDocument = {
+        "Figure"
+    };
+    OptionsMap options = {
+        {"resourceName", optional<string>("definedType")}
+    };
+
+
+    TargetField targetField(TargetFieldType::STANDARD, "defined_type");
+    FieldEncoder lookupListEncoder(
+        optional<TargetField>(targetField),
+        ConverterName::LOOKUP_LIST,
+        {},
+        options
+    );
+    MappingScheme theScheme = {RowMapping(0, lookupListEncoder)};
+
+    MappingOutput result = this->engine->convert(theDocument, theScheme);
+
+    // Expect an empty article object because we haven't defined any other
+    // converters.
+    QJsonObject expectedArticle;
+    vector<string> expectedSourcePaths = {};
+
+    const string expectedResult = R"(
+        {
+           "defined_type": "figure"
+        }
+    )";
+
+
+    ASSERT_THAT(result.getArticleObject(), Eq(deserialize(expectedResult)));
+    ASSERT_THAT(result.getSourcePaths(), Eq(expectedSourcePaths));
+}
