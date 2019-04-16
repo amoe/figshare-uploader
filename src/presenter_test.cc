22cf385f142af3bc8c8affe0770210c6282afd93
David Banks <amoebae@gmail.com>
2019-04-16 16:37:26 +0100

    presenter test

--- src/presenter_test.cc
+++ src/presenter_test.cc
@@ -7,6 +7,8 @@ class PresenterTest: public Test {
 };
 
 TEST_F(PresenterTest, HandlesNewFieldEncoder) {
+    // Set up the presenter with some mocked out collaborators.
+    // Check that it makes the right calls on the model.
     ASSERT_THAT(2 + 2, Eq(5));
 }
 
