0977a8df0a4797a0b0667ed114f211507e71c9f4
David Banks <amoebae@gmail.com>
2019-04-23 22:12:00 +0100

    renaming tests

---
+++ src/mapping_scheme_serializer_test.cc
@@ -0,0 +1,80 @@
+#include <QDebug>
+#include <gmock/gmock.h>
+#include "mapping_types.hh"
+#include "mapping_scheme_serializer.hh"
+#include "test_utility.hh"
+#include "default_field_encoders.hh"
+#include "test_vocabulary.hh"
+
+TEST(MappingSchemeSerializerTest, CanSerializeMappingSchemeWithTitleEncoder) {
+    // Create a simple scheme
+    TargetField targetField(TargetFieldType::STANDARD, "title");
+    FieldEncoder titleEncoder(
+        optional<TargetField>(targetField),
+        ConverterName::STRING,
+        {},
+        {}
+    );
+    MappingScheme theScheme = {titleEncoder};
+
+    MappingSchemeSerializer serializer;
+
+    const string expectedResult = R"(
+        {
+            "rows": [
+                {
+                    "targetField": {
+                        "fieldType": "standard",
+                        "name": "title"
+                    },
+                    "name": "String",
+                    "validationRules": [],
+                    "options": {}
+                }
+            ]
+        }
+    )";
+
+
+    QJsonObject result = serializer.serialize(theScheme);
+
+    qDebug() << result;
+
+    ASSERT_THAT(result, Eq(deserialize(expectedResult)));
+}
+
+
+const string expectedResultForCategoryEncoder = R"(
+    {
+        "rows": [
+            {
+                "targetField": {
+                    "fieldType": "standard",
+                    "name": "categories"
+                },
+                "name": "LookupList",
+                "validationRules": [],
+                "options": {
+                    "delimiter": null,
+                    "resourceName": "category"
+                }
+            }
+        ]
+    }
+)";
+
+
+// The category encoder is chosen because it exercises the various quirks
+// of the serialization format.
+TEST(MappingSchemeSerializerTest, CanSerializeMappingSchemeWithCategoryEncoder) {
+    MappingScheme theScheme = {default_field_encoders::CATEGORY_ENCODER};
+
+    MappingSchemeSerializer serializer;
+    QJsonObject result = serializer.serialize(theScheme);
+
+    qDebug() << result;
+
+    ASSERT_THAT(result, Eq(deserialize(expectedResultForCategoryEncoder)));
+}
+
+
