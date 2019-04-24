3767c867f015c329cedc94474186c4edf7bafd65
David Banks <amoebae@gmail.com>
2019-04-24 17:12:42 +0100

    spdlog

--- src/logging.hh
+++ src/logging.hh
@@ -1,13 +1,8 @@
 #ifndef LOGGING_HH
 #define LOGGING_HH
 
-#include <QtGlobal>
-#include <QMessageLogger>
+#include <spdlog/spdlog.h>
 
-// This macro is very untrustworthy for anything.  Specifically it can't print
-// strings without calling c_str on the argument.   Lame
-
-#define debugf(...) QMessageLogger(__FILE__, __LINE__, Q_FUNC_INFO).debug(__VA_ARGS__)
-void log_init();
+auto console = spdlog::stdout_color_mt("console");
 
 #endif // LOGGING_HH
