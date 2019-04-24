dcf0763ee6ce667dff2fce5816157d04174c9db7
David Banks <amoebae@gmail.com>
2019-04-24 16:24:58 +0100

    hack

--- src/windows_shim.hh
+++ src/windows_shim.hh
@@ -1,6 +1,8 @@
 // See <https://stackoverflow.com/questions/3360548/>
 
 #ifdef _WIN32
+#include <windows.h>
+
 if (AttachConsole(ATTACH_PARENT_PROCESS)) {
     freopen("CONOUT$", "w", stdout);
     freopen("CONOUT$", "w", stderr);
