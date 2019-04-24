c503cacb48b11f2a22e91dc0bc3952cd7d9cfe96
David Banks <amoebae@gmail.com>
2019-04-24 11:46:13 +0100

    upload & part tests

--- src/driver_test.cc
+++ src/driver_test.cc
@@ -80,48 +80,48 @@ public:
 //     driver->handleRow(row, "/fakeinput.xlsx", scheme);
 // }
 
-// TEST_F(DriverTest, canHandleUpload) {
-//     string stemArticle;
+TEST_F(DriverTest, canHandleUpload) {
+    string stemArticle;
 
-//     UploadCreationRequest ucr = ObjectMother::makeUploadCreationRequest();
-//     UploadCreationResponse response = ObjectMother::makeUploadCreationResponse();
-//     FileInfo fileInfo = ObjectMother::makeFileInfo();
-//     UploadContainerInfo uploadContainerInfo = ObjectMother::makeUploadContainerInfo(2);
-//     UploadCommand emptyCommand = ObjectMother::makeUploadCommand();
+    UploadCreationRequest ucr = ObjectMother::makeUploadCreationRequest();
+    UploadCreationResponse response = ObjectMother::makeUploadCreationResponse();
+    FileInfo fileInfo = ObjectMother::makeFileInfo();
+    UploadContainerInfo uploadContainerInfo = ObjectMother::makeUploadContainerInfo(2);
+    UploadCommand emptyCommand = ObjectMother::makeUploadCommand();
 
 
-//     EXPECT_CALL(gateway, createUpload(_, _)).WillOnce(Return(response));
-//     EXPECT_CALL(gateway, getUploadInfo(_)).WillOnce(Return(fileInfo));
-//     EXPECT_CALL(gateway, getUploadContainerInfo(_))
-//      .WillOnce(Return(uploadContainerInfo));
-//     EXPECT_CALL(partPreparer, prepareUpload(_, _, _))
-//         .Times(Exactly(2))
-//         .WillRepeatedly(Return(emptyCommand));
+    EXPECT_CALL(gateway, createUpload(_, _)).WillOnce(Return(response));
+    EXPECT_CALL(gateway, getUploadInfo(_)).WillOnce(Return(fileInfo));
+    EXPECT_CALL(gateway, getUploadContainerInfo(_))
+     .WillOnce(Return(uploadContainerInfo));
+    EXPECT_CALL(partPreparer, prepareUpload(_, _, _))
+        .Times(Exactly(2))
+        .WillRepeatedly(Return(emptyCommand));
 
-//     EXPECT_CALL(gateway, putUpload(_)).Times(Exactly(2));
-//     driver->handleUpload(stemArticle, "something.tiff", ucr);
-// }
+    EXPECT_CALL(gateway, putUpload(_)).Times(Exactly(2));
+    driver->handleUpload(stemArticle, "something.tiff", ucr);
+}
 
-// TEST_F(DriverTest, canHandlePart) {
-//     // We want to check several interactions that are side effecting.
-//     // For instance, it's pretty obvious that putUpload should be called once
-//     // by this code.
-//     // Luckily, FigshareGateway is already an interface.
+TEST_F(DriverTest, canHandlePart) {
+    // We want to check several interactions that are side effecting.
+    // For instance, it's pretty obvious that putUpload should be called once
+    // by this code.
+    // Luckily, FigshareGateway is already an interface.
 
-//     string url;
-//     vector<char> data;
-//     UploadCommand emptyCommand(url, data);
+    string url;
+    vector<char> data;
+    UploadCommand emptyCommand(url, data);
     
 
-//     EXPECT_CALL(partPreparer, prepareUpload(_, _, _))
-//       .WillOnce(Return(emptyCommand));
+    EXPECT_CALL(partPreparer, prepareUpload(_, _, _))
+      .WillOnce(Return(emptyCommand));
 
-//     EXPECT_CALL(gateway, putUpload(_)).Times(AtLeast(1));
+    EXPECT_CALL(gateway, putUpload(_)).Times(AtLeast(1));
 
-//     FileInfo sourceFile = ObjectMother::makeFileInfo();
-//     FilePart partSpec = ObjectMother::makeFilePart(1);
+    FileInfo sourceFile = ObjectMother::makeFileInfo();
+    FilePart partSpec = ObjectMother::makeFilePart(1);
 
-//     driver->handlePart("something.tiff", sourceFile, partSpec);
+    driver->handlePart("something.tiff", sourceFile, partSpec);
     
-//     ASSERT_THAT(2 + 2, Eq(4));
-// }
+    ASSERT_THAT(2 + 2, Eq(4));
+}
