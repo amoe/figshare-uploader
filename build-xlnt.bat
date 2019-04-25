1ae3f55035426bd5deb01077c518303a271beb1e
David Banks <amoebae@gmail.com>
2019-04-25 13:16:53 +0100

    use correct generator

--- build-xlnt.bat
+++ build-xlnt.bat
@@ -2,7 +2,7 @@ md build-xlnt
 cd build-xlnt
 
 set cmake="C:\Program Files\CMake\bin\cmake.exe"
-set cmake_generator="Visual Studio 15 2017"
+set cmake_generator="Visual Studio 15 2017 Win64"
 
 call %cmake% -G %cmake_generator% ../ext/xlnt-1.3.0
 call %cmake% --build . --config Release
