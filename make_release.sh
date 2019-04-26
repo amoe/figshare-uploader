d21b77b25879b6158da7154d1cb2db1b17000ff7
David Banks <amoebae@gmail.com>
2019-04-26 12:55:21 +0100

    release script

---
+++ make_release.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+version_number=$1
+
+if [ -z "$version_number" ]; then
+    echo "Please use a semver number as argument, eg sh make_release.sh '2.0.1'" >&2
+    exit 1
+fi
+
+echo "$version_number" > version
+git commit -am "releasing v${version_number}"
+git tag -a "release-${version_number}" -m "release ${version_number}"
+git push --tags
