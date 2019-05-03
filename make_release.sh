41d549721f393953804d368e2a7d5c64f5b36042
David Banks <amoebae@gmail.com>
2019-05-03 15:46:15 +0100

    release script

--- make_release.sh
+++ make_release.sh
@@ -2,6 +2,9 @@
 
 version_number=$1
 
+last_release=$(git tag | sort -rV | head -n 1)
+echo "The last release was ${last_release}"
+
 if [ -z "$version_number" ]; then
     echo "Please use a semver number as argument, eg sh make_release.sh '2.0.1'" >&2
     exit 1
