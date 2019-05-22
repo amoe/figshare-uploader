38e4d3be02ad1f9025919e96f03007f2b43608c3
David Banks <amoebae@gmail.com>
2019-05-22 10:08:16 +0100

    xlsx CLI validator

--- src/xlsx_validation_driver.cc
+++ src/xlsx_validation_driver.cc
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "xlsx.hh"
 
-int main(int argc, char** argv) {
-    XlsxReader theReader(argv[1]);
+using std::cerr;
+using std::endl;
 
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        cerr << "Please pass the path to one XLSX file on the command line." << endl;
+        return 1;
+    }
+
+    // If something goes wrong with the file, the following code will throw.
+
+    XlsxReader theReader(argv[1]);
     int rowCount = theReader.getRowCount();
     for (int i = 0; i <= rowCount; i++) {
         vector<string> rowData = theReader.rowToString(i);
